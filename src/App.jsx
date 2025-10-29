3ae1952f7b799a5d5c4a69bfeba2ba641f5b0c1b
Praveen <praveenracharla123@gmail.com>
2025-10-29 23:13:14 +0530

    Added react to events code

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import Demo from './components/Demo'
 import './App.css'
 import { Concepts } from './data/data.js'
+import TabButton from './components/TabButton.jsx'
 
 function App() {
   const [count, setCount] = useState(2)
 
   const increment = ()=>setCount(count+1);
   const decrement = ()=>setCount(count-1);
+  const [tabContent, setTabContent] = useState("Please Click any Button!");
+  const handleClick = (selectedConcpet)=>{
+    console.log(selectedConcpet);
+    setTabContent("Clicked");
+  };
 
 
   return (
@@ -20,8 +26,16 @@ function App() {
               id = {Concepts[0].id}
         />
         <Demo {...Concepts[1]}/>
-        <Demo {...Concepts[2]}/>
-        <Demo {...Concepts[3]}/>
+        <section>
+          <h3>Examples</h3>
+          <menu className="tab-menu">
+            <TabButton onSelect={()=>handleClick('components')}>Components</TabButton>
+            <TabButton onSelect={()=>handleClick('JSX')}>JSX</TabButton>
+            <TabButton onSelect={()=>handleClick('Props')}>Props</TabButton>
+            <TabButton onSelect={()=>handleClick('State')}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </div>
     </>
   )
